4ef38e574a3225b8ad017a5654a2d3d60a67a2c0
vidbhardwaj <vid.bhardwaj@gmail.com>
2026-03-04 19:37:37 +0530

    Adding python files to basics

---
+++ basics/operators_conditions.py
@@ -0,0 +1,15 @@
+#operators
+a, b = 5, 2
+print("Addition:", a + b)
+print("Substartion:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+#conditions
+if a > b:
+    print("a is greater than b")
+elif a == b:
+    print("a equals b")
+else:
+    print("b is greater than a")
---
+++ basics/variables_datatypes.py
@@ -0,0 +1,10 @@
+#variable and datatypes
+x = 10 #integer
+y = 3.14 #float
+name = "Vid" #string
+is_active = True #boolean
+#type casting
+a = int(y) #float to int
+b = str(x) #int to string
+print(type(x), type(y), type(name), type(is_active))
+print("Casting:", a, b)
