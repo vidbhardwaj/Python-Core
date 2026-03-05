bc39da8d00eda09f3e02b7a141b3d887809cf8d9
vidbhardwaj <vid.bhardwaj@gmail.com>
2026-03-05 11:26:17 +0530

    python functions

---
+++ basics/functions.py
@@ -0,0 +1,9 @@
+#function define
+def greet(name):
+    return f"Hello, {name}!"
+#parameterised function
+def add(x, y):
+    return x + y
+#calling the function
+print(greet("Vid"))
+print("Sum:", add(5, 7))
